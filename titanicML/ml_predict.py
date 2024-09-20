54e2b57aaedc610bb503a2a05397afddab006dfd
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2024-09-19 22:00:50 -0400

    added an if statement to the prediction model to display if the person survived or not in a string

--- titanicML/ml_predict.py
+++ titanicML/ml_predict.py
@@ -3,4 +3,10 @@ def prediction_model(pclass, sex, age, sibsp, parch, fare, embarked, title):
     x = [[pclass, sex, age, sibsp, parch, fare, embarked, title]]
     randomforest = pickle.load(open('titanic_model.sav', 'rb'))
     prediction = randomforest.predict(x)
+    if prediction == 0:
+        prediction = 'Not survived'
+    elif prediction == 1:
+        prediction = 'Survived'
+    else:
+        prediction = 'Error'
     return prediction
